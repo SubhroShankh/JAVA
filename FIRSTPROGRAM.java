58d1a53402eb87ec4287857c28f68a675134b202
SubhroShankh <subhroshankh389@gmail.com>
2025-06-23 02:21:05 -0700

    adding my java files to github

---
+++ FIRSTPROGRAM.java
@@ -0,0 +1,16 @@
+
+// print code
+public class FIRSTPROGRAM {
+    public static void main(String[] args){
+    System.out.println("java");
+    System.out.print(" is a programming language.\n");
+    System.out.print("It is used to develop applications.\n");
+    // if we write System.out.println, it will print the text and move to the next line
+    // if we write System.out.print, it will print the text and stay on the same line
+    // if we write \n in print text, it will move to the next line
+    // it is mandatory to write main method in every java program because it is the entry point of the program
+    // public is written so that JVM can access the main method 
+}
+}
+  
+ 
---
+++ Practice1.java
@@ -0,0 +1,11 @@
+public class Practice1 {
+    public static void main(String[] args) {
+        System.out.print("Good Morning \n");
+        System.out.println("subscribe");
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+    }
+}

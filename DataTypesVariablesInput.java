ff6dfb0c05420ba2351509e306d3a24b63eafc80
SubhroShankh <subhroshankh389@gmail.com>
2025-06-24 04:06:30 -0700

    Adding new files

---
+++ DataTypesVariablesInput.java
@@ -0,0 +1,29 @@
+public class DataTypesVariablesInput {
+    public static void main(String[] args){
+        int myNumber = 865;
+        int yourNumber;
+        yourNumber = 856;
+        System.out.println(yourNumber);
+        yourNumber = 85;
+        System.out.println(yourNumber);
+        // we use int for whole numbers, it can be positive or negative
+        
+        boolean isVegetarian = true;
+        System.out.println(isVegetarian);
+        // we use boolean for true/false values
+
+        float myFloat = 5.0f;
+        System.out.println(myFloat);
+        // we use float for decimal numbers, the 'f' is necessary to indicate it's a float
+        float yourFloat = 6f;
+        System.out.println(yourFloat);
+
+        double myDouble = 6.8484848;
+        System.out.println(myDouble);
+        // we use double for larger decimal numbers, it has more precision than float
+
+        String wishes = "Good Morning";
+        System.out.println(wishes);
+
+    }
+}

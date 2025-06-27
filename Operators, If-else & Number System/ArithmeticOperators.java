eae040042f8e9164dd0736c8225095a9922df9b5
SubhroShankh <subhroshankh389@gmail.com>
2025-06-27 04:21:24 -0700

    FILES

---
+++ Operators, If-else & Number System/ArithmeticOperators.java
@@ -0,0 +1,17 @@
+public  class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int sum = a + b; // Addition
+        int difference = a - b; // Subtraction
+        int product = a * b; // Multiplication
+        int quotient = b / a; // Division
+        int remainder = b % a; // Modulus
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
+    
+}

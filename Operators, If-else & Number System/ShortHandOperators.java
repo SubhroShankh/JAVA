63dad507f9f7638751cd8cc4c513d86a8a72127b
SubhroShankh <subhroshankh389@gmail.com>
2025-06-28 22:11:50 +0530

    Files

---
+++ Operators, If-else & Number System/ShortHandOperators.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ShortHandOperators {
+    public static void main(String[] args) {
+        int a = 5;
+        Scanner input = new Scanner(System.in);
+        int x1= input.nextInt();
+        a += x1; // a = a + x1
+        System.out.println(a);
+        int x2 = input.nextInt();
+        a -= x2; // a = a - x2
+        System.out.println(a);
+        int x3 = input.nextInt();
+        a *= x3; // a = a * x3
+        System.out.println(a);
+        int x4 = input.nextInt();
+        a /= x4; // a = a / x4
+        System.out.println(a);
+        int x5 = input.nextInt();
+        a %= x5; // a = a % x5
+        System.out.println(a); 
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

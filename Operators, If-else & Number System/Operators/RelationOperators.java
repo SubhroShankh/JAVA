de0f58c1ae4ee48b0ccc9ec9828ee04b9ec580ba
SubhroShankh <subhroshankh389@gmail.com>
2025-07-01 21:51:49 +0530

    Files

--- Operators, If-else & Number System/Operators/RelationOperators.java
+++ Operators, If-else & Number System/Operators/RelationOperators.java
@@ -8,12 +8,12 @@ public class RelationOperators {
         System.out.print("Enter second number: ");
         int num2 = input.nextInt();
 
-        System.out.println("num1 == num2: " + (num1 == num2));
-        System.out.println("num1 != num2: " + (num1 != num2));
-        System.out.println("num1 > num2: " + (num1 > num2));
-        System.out.println("num1 < num2: " + (num1 < num2));
-        System.out.println("num1 >= num2: " + (num1 >= num2));
-        System.out.println("num1 <= num2: " + (num1 <= num2));
+        System.out.println("num1 == num2: " + (num1 == num2)); // Check if num1 is equal to num2 = true if both numbers are equal
+        System.out.println("num1 != num2: " + (num1 != num2)); // Check if num1 is not equal to num2 = true if both numbers are not equal
+        System.out.println("num1 > num2: " + (num1 > num2)); // Check if num1 is greater than num2 = true if num1 is greater than num2
+        System.out.println("num1 < num2: " + (num1 < num2)); // Check if num1 is less than num2 = true if num1 is less than num2
+        System.out.println("num1 >= num2: " + (num1 >= num2)); // Check if num1 is greater than or equal to num2 = true if num1 is greater than or equal to num2
+        System.out.println("num1 <= num2: " + (num1 <= num2)); // Check if num1 is less than or equal to num2 = true if num1 is less than or equal to num2
     }
 }
   

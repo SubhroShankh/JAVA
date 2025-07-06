7536a925d5dbb47b1e14be300a2a2da5d0304a7c
SubhroShankh <subhroshankh389@gmail.com>
2025-07-06 13:07:12 +0530

    Files

--- While loop , Methods & Array/Functions/Functions.java
+++ While loop , Methods & Array/Functions/Functions.java
@@ -1,5 +1,11 @@
 public class Functions {
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("Good Morning Visitor.");
+        greetings(); 
     }
+
+public static void greetings() {
+    System.out.println("Hello");
+}
+
 }

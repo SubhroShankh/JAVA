be6d34e01ca06567ede10bbc297f3b8ea6b12835
SubhroShankh <subhroshankh389@gmail.com>
2025-07-07 11:54:22 +0530

    Files

---
+++ While loop , Methods & Array/Functions/patternFunction.java
@@ -0,0 +1,26 @@
+
+
+public class patternFunction {
+     public static void main(String[] args) {
+        pattern1();
+        pattern2();
+     }
+     public static void pattern1() {
+        System.out.print("Good Morning \n");
+        System.out.println("User");
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+     }
+     public static void pattern2() {
+        System.out.print("Good Morning \n");
+        System.out.println("User");
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+     }
+}
---
+++ While loop , Methods & Array/Functions/patternPrinting.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class patternPrinting {
+    public static void main(String[] args) {
+        System.out.println("Welcome to star printing programme.\n ");
+        userInput();
+    }
+    public static void userInput() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter how many column you want to print: ");
+        int a = input.nextInt();
+        int b = 0;
+          
+
+        while (b < a) {
+            System.out.print("*");
+            int i = 0;
+            while (i < b) {
+                System.out.print("*");
+                i++;
+
+            }
+            System.out.println();
+            b++;
+        }
+    }
+}

0acbc7a941d93dd30c6489d02b944bc48ebb5332
SubhroShankh <subhroshankh389@gmail.com>
2025-08-02 01:28:33 +0530

    Files

--- Class and Objects/Car.java
+++ Class and Objects/Car.java
@@ -1,60 +1,69 @@
 
-
 public class Car {
-    
-    static int noOfCarSold ;
-    int noOfWheels;
-    String colour;
-    float maxSpeed;
-    float currentFuelInLiters;
-    int noOfSeats;
-    
+
+    // Static variable shared across all instances (tracks total cars sold)
+    static int noOfCarSold;
+
+    // Instance variables unique to each Car object
+    int noOfWheels;                 
+    String colour;                  
+    float maxSpeed;                 
+    float currentFuelInLiters;      
+    int noOfSeats;                  
+
+    // Static block: runs only once when the class is first loaded into memory
     static {
         noOfCarSold = 0;
         System.out.println("I am in Static Block.");
     }
 
+    // Instance initializer block: runs every time a new object is created *before* the constructor
     {
-        noOfCarSold++;
+        noOfCarSold++;  
         System.out.println("I am in init Block.");
     }
 
-    Car(String colour){
+    // Constructor with a colour parameter
+    Car(String colour) {
         noOfWheels = 4;
         this.colour = colour;
         maxSpeed = 150;
         currentFuelInLiters = 2;
-        noOfSeats = 5;  
+        noOfSeats = 5;
     }
 
-    Car(){
-        this("Black");
+    // Default constructor: assigns colour as "Black" and sets fuel level to 5
+    Car() {
+        this("Black");             // Calls the above constructor
         currentFuelInLiters = 5;
     }
 
-    public Car start(){
+    // Method to start the car
+    public Car start() {
         if (currentFuelInLiters == 0) {
             System.out.println("Car is out of fuel , can not start");
-        } else if(currentFuelInLiters < 5){
-            System.out.println("Car is in resevered mode ,please refuel.");
-            
+        } else if (currentFuelInLiters < 5) {
+            System.out.println("Car is in reserved mode , please refuel.");
         } else {
-                    System.out.println("Car has started... bruhhh...");
-                    currentFuelInLiters--;
+            System.out.println("Car has started... bruhhh...");
+            currentFuelInLiters--;     
         }
-        return this;
+        return this;                   
     }
 
+    // Method to drive the car
     public void drive() {
-        currentFuelInLiters--;
+        currentFuelInLiters--;         
         System.out.println("Car is driving.");
-        }
-
-    public void addFuel(float currentFuelInLiters){
-        this.currentFuelInLiters += currentFuelInLiters; 
     }
 
-    public float getCurrentFuelLevel(){
+    // Method to add fuel to the car
+    public void addFuel(float currentFuelInLiters) {
+        this.currentFuelInLiters += currentFuelInLiters;
+    }
+
+    // Getter method: returns current fuel level
+    public float getCurrentFuelLevel() {
         return currentFuelInLiters;
     }
-}    
+}
--- Class and Objects/Driver.java
+++ Class and Objects/Driver.java
@@ -1,19 +1,28 @@
-
-
+// The Driver class represents a person who may be allowed to drive
 public class Driver {
 
+    // Static variable shared across all Driver instances
+    // Defines the minimum age required to drive (e.g., 18)
     static int minAgeForDriving;
 
-    String name;
-    int age ;
-    String dateOfLicence;
+    // Instance variables - unique for every Driver object
+    String name;             // Driver's name
+    int age;                 // Driver's age
+    String dateOfLicence;    // License issue date
 
-    public boolean isAllowedToDrive(){
+    // Method to check if the driver is allowed to drive
+    // Returns true if age is greater than or equal to minimum age
+    public boolean isAllowedToDrive() {
         return this.age >= minAgeForDriving;
     }
 
+    // Main method - entry point of the program
     public static void main(String[] args) {
-        /*Car myCar = new Car();
+
+        //  This section is commented out. It creates a Car object,
+        // adds fuel, drives it multiple times, then checks fuel level.
+        /*
+        Car myCar = new Car();
         myCar.addFuel(6);
         myCar.drive();
         myCar.drive();
@@ -24,20 +33,29 @@ public class Driver {
         myCar.drive();
         myCar.drive(); 
         myCar.drive();
-        System.out.println(myCar.getCurrentFuelLevel()); */
+        System.out.println(myCar.getCurrentFuelLevel());
+        */
 
-        Car swift = new Car("pink");
-        Car thar  = new Car();
-        //swift.addFuel(6);
-        /*Car startedCar = swift.start();
-        startedCar.drive(); */
-        swift.start().drive(); 
-        System.out.println(swift.colour);
-        System.out.println(thar.colour);
+        //  Creating two Car objects
+        Car swift = new Car("pink");   // Car with specified colour
+        Car thar  = new Car();         // Car with default colour ("Black")
 
+        //  This line is commented out, it would add extra fuel to swift
+        // swift.addFuel(6);
 
-        /*Driver myDriver = new Driver();
+        // Start swift car and immediately drive it
+        swift.start().drive();         // Method chaining (start then drive)
+
+        // Print colour of the cars
+        System.out.println(swift.colour);  // Outputs "pink"
+        System.out.println(thar.colour);   // Outputs "Black"
+
+        //  This section is commented out, initializes a Driver
+        // and assigns a license date. Then prints minimum age.
+        /*
+        Driver myDriver = new Driver();
         myDriver.dateOfLicence = "1/jan/2025";
-        System.out.println(minAgeForDriving);  */
+        System.out.println(minAgeForDriving);
+        */
     }
-} 
+}

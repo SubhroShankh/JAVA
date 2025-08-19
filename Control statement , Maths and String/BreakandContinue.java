2f10a6d1f2d223450917ef8464887efce4669b42
SubhroShankh <subhroshankh389@gmail.com>
2025-08-19 20:24:20 +0530

    Files

---
+++ Control statement , Maths and String/BreakandContinue.java
@@ -0,0 +1,29 @@
+public class BreakandContinue {
+    public static void main(String[] args) {
+        /*System.out.println("Before Loop");
+        for(int i = 1; i<1000 ; i++ ){
+            
+                if (i == 101) {
+                    break;
+                }
+                System.out.println(i);
+            
+            
+        }
+        System.out.println("Out ofLoop"); */
+
+
+
+        System.out.println("Before Loop");
+        for(int i = 1; i<10 ; i++ ){
+            
+                if (i == 5) {
+                    continue;
+                }
+                System.out.println(i);
+            
+            
+        }
+        System.out.println("Out ofLoop");
+    }
+}
---
+++ Control statement , Maths and String/forEachLoop.java
@@ -0,0 +1,10 @@
+public class forEachLoop {
+    public static void main(String[] args) {
+        String[] array = new String[]{
+            "Ram" ,"Shayam","Mohan","Sohan","Sita","Gita"
+        };
+        for(String peopleName :array){
+            System.out.println(peopleName);
+        }
+    }
+}

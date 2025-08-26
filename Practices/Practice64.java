55a067cbeda7f135862bb43b78fab19e73ba2fc8
SubhroShankh <subhroshankh389@gmail.com>
2025-08-26 12:59:50 +0530

    Files

---
+++ Practices/Practice64.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class PalindromeRecursion{
+    public static void main(String[] args) {
+        System.out.println("Welcome to palindrome checker.\n");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the string to be checked: ");
+        String str = input.nextLine();
+        System.out.println("Your entered string is "+(isPalindrome(str)? "palindrome" :"not a palindrome" ));
+
+    }
+
+    public static boolean isPalindrome(String palindrome){
+        if (palindrome.length() <= 1) {
+            return true;
+        }
+        int lastPas = palindrome.length() -1;
+        if(palindrome.charAt(0) != palindrome.charAt(lastPas)){
+            return false;
+        }
+        String newString = palindrome.substring(1, lastPas);
+        return isPalindrome(newString);
+    }
+}

ac00940495e2f6db0e19cb448209b0306c6f2abf
SubhroShankh <subhroshankh389@gmail.com>
2025-12-24 21:01:59 +0530

    Files

--- DSA-JAVA/Strings.java
+++ DSA-JAVA/Strings.java
@@ -10,28 +10,64 @@ public class Strings {
     }
 
     public static void main(String[] args) {
-        char arr[] = { 'a', 'b', 'c', 'd' };
-        String str = new String("xyz");
-        String str2 = "abcd";
+        // char arr[] = { 'a', 'b', 'c', 'd' };
+        // String str = new String("xyz");
+        // String str2 = "abcd";
 
         // Strins are immutable in java
-        Scanner sc = new Scanner(System.in);
-        String name;
+        // Scanner sc = new Scanner(System.in);
+        // String name;
         // name = sc.next();
         // System.out.println(name);
         // name = sc.nextLine();
         // System.out.println(name);
 
-        String fullName = "Tony stark";
-        System.out.println(fullName.length());
+        // String fullName = "Tony stark";
+        // System.out.println(fullName.length());
 
         // concatination
-        String firstName = "Subhro ";
-        String lastName = "Shankh";
-        String totalName;
-        totalName = firstName + lastName;
-        System.out.println(totalName);
-        System.out.println(totalName.charAt(1));
-        printLetters(totalName);
+        /*
+         * String firstName = "Subhro ";
+         * String lastName = "Shankh";
+         * String totalName;
+         * totalName = firstName + lastName;
+         * System.out.println(totalName);
+         * System.out.println(totalName.charAt(1));
+         * printLetters(totalName);
+         */
+
+        String s1 = "Tony";
+        String s2 = "Tony";
+        String s3 = new String("Tony");
+        if (s1 == s2) {
+            System.out.println("Strings are equal.");
+        } else {
+            System.out.println("Strings are not equal.");
+        }
+
+        if (s1 == s3) {
+            System.out.println("Strings are equal.");
+        } else {
+            System.out.println("Strings are not equal.");
+        }
+
+        if (s1.equals(s3)) {
+            System.out.println("Strings are equal.");
+        } else {
+            System.out.println("Strings are not equal.");
+        }
+
+        // Substring
+        String str = "Hello world";
+        System.out.println(subString(str, 0, 5));
+        System.out.println(str.substring(0, 5));
+    }
+
+    public static String subString(String str, int si, int ei) {
+        String subString = "";
+        for (int i = si; i < ei; i++) {
+            subString += str.charAt(i);
+        }
+        return subString;
     }
 }
